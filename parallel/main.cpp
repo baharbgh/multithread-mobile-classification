e6ca9f49e55c1308d46f75d70b50372ae0fb4f8c
kooranifar <kooranifar@gmail.com>
2020-12-15 19:38:48 +0330

    folders created

---
+++ parallel/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <chrono>
+#include <stdio.h>
+#include <pthread.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
